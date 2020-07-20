65dba70021756418acd2c64c33783b32c0e96655
Gustavo Magalhães <mpm.gustavo@gmail.com>
2020-07-20 01:51:11 +0100

    removed typo and switch user model

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from .models import CustomUser as User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__' #('username','email', 'date_joined', 'last_login', 'password')
-        read_only_fields = ('date_joined', 'last_login',)
+        fields = '__all__'
+        read_only_fields = ('date_joined', 'last_login', )
         extra_kwargs= { 'password': {'write_only': True}, 'first_name': {'write_only': True}, 'last_name': { 'write_only': True}}
