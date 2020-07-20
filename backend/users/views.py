d5d7820294ad8864a49463a33d677e1045e14707
Gustavo Magalhães <mpm.gustavo@gmail.com>
2020-07-20 01:51:46 +0100

    add modelviewset with custom permissions

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets, response, status
+from .models import CustomUser as User
+from .serializers import UserSerializer
 
-# Create your views here.
+class CustomPermission(permissions.BasePermission):
+    '''
+    If is admin -> allow all (GET, PUT, POST, DELETE)
+    If user is owner -> allow RETRIEVE, PATCH
+    '''
+    def has_permission(self, request, view):
+        if view.action == 'list':
+            return request.user.is_staff
+        else:
+            return True
+    def has_object_permission(self, request, view, obj):
+        if view.action in ['retrieve', 'partial_update']:
+            print(request)
+            return obj == request.user or request.user.is_staff
+        else:
+            return request.user.is_staff
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (CustomPermission,)
