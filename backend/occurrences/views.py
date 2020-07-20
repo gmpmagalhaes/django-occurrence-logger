5b92413253a46ffcd002c2b912a74260de8ee685
Gustavo Magalhães <mpm.gustavo@gmail.com>
2020-07-20 05:18:13 +0100

    add viewsets

--- backend/occurrences/views.py
+++ backend/occurrences/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .serializers import OccurrenceSerializer, UpdateOccurrenceSerializer
+from .models import Occurrence
+from .filters import OccurrenceFilter
 
-# Create your views here.
+class CustomPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated or request.user.is_staff
+    def has_object_permission(self, request, view, obj):
+        return view.action in ['retrieve', 'partial_update', 'destroy'] and request.user.is_staff or request.user == obj
+
+class OccurrenceViewSet(viewsets.ModelViewSet):
+    queryset = Occurrence.objects.all()
+    serializer_class = OccurrenceSerializer
+    permission_classes = (CustomPermission,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = OccurrenceFilter
+
+    def get_serializer_class(self):
+        serializer_class = self.serializer_class
+        if self.request.method == 'PUT':
+            serializer_class = UpdateOccurenceSerializer
+        return serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
